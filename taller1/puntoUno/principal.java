63c2ace4f8cc31c95343264307a99e913a8ede5c
Sebastián López Montenegro <110605842+Sxl07@users.noreply.github.com>
2023-02-24 10:29:51 -0500

    Add files via upload
    añadiendo solo el punto uno del taller1

---
+++ taller1/puntoUno/principal.java
@@ -0,0 +1,11 @@
+package taller1.puntoUno;
+
+class principal {
+    public static void main(String[] args) {
+        //aqui se crea una nueva persona con la clase Persona.(Punto A del primer punto)
+        Persona nuevaPersona = new Persona("sebastian", "lopez");
+        //aqui se imprime la nueva persona que se creó con la clase Persona(punto B del primer punto)
+        System.out.println(nuevaPersona);
+        //en la carpeta se encuentra la captura con la ejecucion del programa.
+    }
+}
