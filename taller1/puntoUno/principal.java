723ab7997fba4ddac73aaf3518bf627193ce2df3
Sebastián López Montenegro <110605842+Sxl07@users.noreply.github.com>
2023-02-24 10:32:37 -0500

    Update principal.java

--- taller1/puntoUno/principal.java
+++ taller1/puntoUno/principal.java
@@ -6,6 +6,6 @@ class principal {
         Persona nuevaPersona = new Persona("sebastian", "lopez");
         //aqui se imprime la nueva persona que se creó con la clase Persona(punto B del primer punto)
         System.out.println(nuevaPersona);
-        //en la carpeta se encuentra la captura con la ejecucion del programa.
+        //en la carpeta se encuentra la captura con la ejecucion del programa. Ese sería el punto 2 del taller
     }
 }
